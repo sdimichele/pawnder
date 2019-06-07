e8f2c7e9f8266ea89fad582b1566df08df6bb76a
sdimichele <sabrinadimichele1992@gmail.com>
2019-06-07 17:37:25 -0500

    added links and did show page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/animals/:id">Select An Animal </router-link>
+      <router-link to="/animals">Select An Animal </router-link>
+      
     </div>
     <router-view/>
   </div>
--- src/views/AnimalsAll.vue
+++ src/views/AnimalsAll.vue
@@ -1,10 +1,23 @@
 <template>
-   <div class="animals-show">
+  <div class="animals-index">
     <div class="container">
-    
-
-         <div class="row mt-5">
-        <div class="col-md-5 offset-4">
+      
+     <h1 class="page-title">All Animals</h1>
+       <!-- <img v-bind:src=" animal['photos'][0]['medium'] ">
+       <ul>
+         <a class="btn btn-primary" href="/animals/:id" role="button">More Info</a>
+         <li>{{ animal['photos'][0]['full'] }}</li>
+         <li>Name: {{ animal["name"] }}</li>
+         <li>Type: {{ animal["type"] }}</li>
+         <li>Type: {{ animal["breeds"] }}</li>
+         <li>Age: {{ animal["age"] }}</li>
+         <li>Attributes: {{ animal["attributes"] }}</li>
+         <li>Personality traits: {{ animal["tags"] }}</li>
+         <li>Type: {{ animal["age"] }}</li>
+       </ul> -->
+      <div class="row mt-5">
+        <div class="col-md-4" v-for="animal in animals">
+           <router-link v-bind:to="'/animals/' + animal['id']"><button> Click me!!!!!</button></router-link>
          <div class="card border-dark mb-5">
            <img v-bind:src="animal['photos'][0]['full']" class="card-img-top" v-bind:alt="animal['name']">
            <div class="card-body">
@@ -19,16 +32,17 @@
              <li class="list-group-item">Age: {{animal['age']}} </li>
            </ul>
            <div class="card-body card-links">
-             <a v-bind:href="animal['url']" class="card-link">Adopt Me</a>
+             <a href="#" class="card-link">Card link</a>
+
            </div>
          </div>
         </div>
+         
       </div>
     </div>
   </div>
-</template>
 
-    
+</template>
 
 <style>
   .card-img-top {
@@ -38,7 +52,6 @@
  .card {
   text-align: center;
   height: 850px;
-
  }
 
  .page-title {
@@ -48,26 +61,27 @@
 
  </style>
 
-
  <script>
  var axios = require('axios');
+
  export default {
    data: function() {
      
      return {
-             animal: []
-            };
+       animals: []
+     };
    },
+
    created: function() {
 
-    axios.get('https://api.petfinder.com/v2/animals/' + this.$route.params.id,
+    axios.get("https://api.petfinder.com/v2/animals?type=dog&location=25601&limit=21&distance=20",
         {headers: {
             "Authorization" : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjdkYjYwMjJiYjU4MDcxYTY3ZGJlMTM5NDgwZjM1YTljMzcyM2FjZDFmNzNkNzU2NzRjNmFiN2M1NjdiZDhiZGQxM2Q3MDU1NTExOTcxZDY4In0.eyJhdWQiOiJwS0o3Rlp3RHVPZldRcFprU2R6RU9rOTZQbE1CYWh4UVRxS0s4NFV4MEd5OU5IcEpJRiIsImp0aSI6IjdkYjYwMjJiYjU4MDcxYTY3ZGJlMTM5NDgwZjM1YTljMzcyM2FjZDFmNzNkNzU2NzRjNmFiN2M1NjdiZDhiZGQxM2Q3MDU1NTExOTcxZDY4IiwiaWF0IjoxNTU5OTQwOTU0LCJuYmYiOjE1NTk5NDA5NTQsImV4cCI6MTU1OTk0NDU1NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.mQXqeDWOKBxAy8NGZo6Qvutxmx8OIFh7YiX5OvEUYd61b1NpoJL3G9EMQg3SG3F0r-Ug2i3-I1Sjb9F2VgO87ZQPd8R8yaMG24CnW159cjfhO0rPo0YLk9E7zIC26XDuepjYCrzyDo3P7v_XgjLZZL0SMxpYl-tRmlYbh5vwIZlEFJhMHfA8zS3r23dBurAsSZhXiVNMRWKeSHSEB31QgP7f4Z22AIOgfrXIZTmULfqPlOIB3XZV1Ky9jeO2yFUzd8wkM4s1xWVZCHrs_7hXS1AaU92ETHVWkUg0tNOfLbd1WngxGe6hlJOeMJtpF08fhY-m5gBveFdsJLYK3p2u7Q"}
         }
     ).then(response => {
         console.log(response.data);
-        this.animal = response.data.animal;
-        console.log(this.animal);
+        this.animals = response.data.animals;
+        console.log(this.animals);
      });
    },
    methods: {}
